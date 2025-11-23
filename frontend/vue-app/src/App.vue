b586f5e0c580de2f62a7078c343c9e097376800b
Muddasir <muddasirmd2@gmail.com>
2025-11-23 19:31:25 +0500

    UI changes

--- frontend/vue-app/src/App.vue
+++ frontend/vue-app/src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-    <h1 class="text-2xl font-bold mb-4">AI Text Summarizer</h1>
-    <textarea v-model="text" class="w-full max-w-lg p-3 rounded border" rows="8" placeholder="Paste your text here..."></textarea>
-    <button @click="summarize" class="bg-blue-500 text-white px-4 py-2 rounded mt-3">Summarize</button>
-    <div v-if="summary" class="mt-4 bg-white shadow p-4 rounded w-full max-w-lg">
-      <h2 class="font-semibold mb-2">Summary:</h2>
-      <p>{{ summary }}</p>
+  <div
+    class="flex flex-col justify-center items-center gap-3 w-screen h-screen overflow-auto bg-linear-to-r from-gray-100 to-gray-300">
+
+    <h1 class="text-3xl font-bold">AI Text Summarizer</h1>
+
+    <div class="flex flex-col gap-3 w-[50%] p-6 shadow-md rounded-2xl bg-white">
+
+      <div class="flex flex-col items-end gap-3">
+        <textarea v-model="text"
+          class="w-full h-[60px] max-h-[300px] p-3 resize-none overflow-y-auto rounded-lg bg-gray-100"
+          oninput="this.style.height='auto'; this.style.height=this.scrollHeight+'px'"
+          placeholder="Type a prompt and let AI to summerize it for you"></textarea>
+
+        <button @click="summarize" :disabled="loading"
+          :class="[loading ? 'opacity-50 pointer-events-none' : '', 'flex w-fit px-3 py-2 rounded-2xl text-lg font-medium cursor-pointer text-white bg-linear-to-r from-purple-600 to-fuchsia-600']">
+          Summerize
+        </button>
+
+      </div>
+
+      <!--  --- Summary Section --- -->
+      <div v-if="summary" class="flex flex-col gap-2 max-h-[300px] overflow-auto p-2">
+        <h2 class="text-lg font-semibold">Summary:</h2>
+        <p>{{ summary }}</p>
+
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -14,8 +34,12 @@
 import { ref } from 'vue'
 const text = ref('')
 const summary = ref('')
+const loading = ref(false);
 
 async function summarize() {
+  loading.value = true;
+  summary.value = 'Thinking...';
+
   const res = await fetch('http://localhost:8000/api/summarize/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -23,5 +47,6 @@ async function summarize() {
   })
   const data = await res.json()
   summary.value = data.summary || data.error
+  loading.value = false;
 }
 </script>
